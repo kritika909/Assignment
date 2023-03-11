e6b7b910e4a5d3a02d64a4ac8c12029c5adf8b85
kritika909 <126762803+kritika909@users.noreply.github.com>
2023-03-11 18:54:31 +0530

    Update assign1.c

--- week1/assign1.c
+++ week1/assign1.c
@@ -4,17 +4,17 @@ int main()
     int x;
     for(x=1; x<=100; x++)
     {
-        if(x%3==0)
+        if(x%3==0 & x%5==0)
+        {
+            printf("FizzBuzz\n");
+        }
+        else if(x%3==0)
         {
             printf("Fizz\n");
         }
         else if(x%5==0)
         {
             printf("Buzz\n");
-        }
-        else if(x%3==0 & x%5==0)
-        {
-            printf("FizzBuzz\n");
         }
         else
         {
